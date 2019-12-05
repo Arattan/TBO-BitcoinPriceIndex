fa51cb943df9dbce702d1e7434e950d4d25a1aa9
Arattan <amanrattan21@gmail.com>
2019-12-05 15:47:04 +0100

    Add BitcoinPrice class time & price fields, and getter methods

--- app/src/main/java/com/example/android/tbo_bitcoinpriceindex/BitcoinPrice.java
+++ app/src/main/java/com/example/android/tbo_bitcoinpriceindex/BitcoinPrice.java
@@ -1,8 +1,19 @@
 package com.example.android.tbo_bitcoinpriceindex;
 
+import com.squareup.moshi.Json;
+
 import java.util.Date;
 
 public class BitcoinPrice {
-    private double price;
     private Date time;
+    @Json(name = "bpi") private double price;
+
+
+    public Date getTime() {
+        return time;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
